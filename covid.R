899396f792b5dee161994f7aa99e56c09be299e1
zxrey <68795117+zxrey@users.noreply.github.com>
2020-09-18 19:35:05 -0500

    limpieza de datos

--- covid.R
+++ covid.R
@@ -6,34 +6,90 @@ library(ggplot2)
 library(plotly)
 library(extrafont)
 
-
+# actualizado a 18/08/2020 https://www.datosabiertos.gob.pe/dataset/fallecidos-por-covid-19-ministerio-de-salud-minsa
+# actualizado a 18/08/2020 https://www.datosabiertos.gob.pe/dataset/casos-positivos-por-covid-19-ministerio-de-salud-minsa
 getwd()
-fallecidos<- readxl::read_xlsx("fallecidos_covid.xlsx")
-fallecidos$FECHA_FALLECIMIENTO<-ymd(as.character(fallecidos$FECHA_FALLECIMIENTO))
-fallecidos$FECHA_NAC<-ymd(as.character(fallecidos$FECHA_NAC))
+fallecidos_covid<- readxl::read_xlsx("fallecidos_covid.xlsx")
+positivos_covid <- readxl::read_xlsx("positivos_covid.xlsx")
 
-View(fallecidos)
-sapply(fallecidos,class)
-table(fallecidos$SEXO)
-table(fallecidos$DEPARTAMENTO)
-sapply(fallecidos,class)
+#--------------------------------------------
+# hacemos una limpieza de los datos
+## Hacemos una observacion de los datos
+#LINK:https://campus.datacamp.com/courses/cleaning-data-in-r/common-data-problems?ex=1
+glimpse(fallecidos_covid)
+glimpse(positivos_covid)
+class(fallecidos_covid$SEXO)
+class(fallecidos_covid$SEXO)
+assert_is_factor(positivos_covid$SEXO) #si sale algun error es que no es factor y si no sale algo es que si es factor
+is.factor(positivos_covid$SEXO)
+
+#transformamos las variables que contienen fechas 
+#LINK: https://campus.datacamp.com/courses/working-with-data-in-the-tidyverse/transform-your-data?ex=7
+ymd("20200420")
+fallecidos_covid <- fallecidos_covid %>% 
+  mutate(FECHA_FALLECIMIENTO=ymd(FECHA_FALLECIMIENTO),FECHA_NAC=ymd(FECHA_NAC))
+positivos_covid <- positivos_covid %>% 
+  mutate(FECHA_RESULTADO=ymd(FECHA_RESULTADO))
+
+#ahora transformamos a factor algunas variables
+fallecidos_covid$SEXO <- as.factor(as.character(fallecidos_covid$SEXO))
+positivos_covid$SEXO <- as.factor(as.character(positivos_covid$SEXO))
+
+positivos_covid$METODODX <- as.factor(as.character(positivos_covid$METODODX))
+
+is.factor(positivos_covid$METODODX)
+is.factor(positivos_covid$SEXO)
+
+levels(fallecidos_covid$SEXO)
+
+#buscamos NA 
+
+sapply(fallecidos_covid, function(x) sum(is.na(x)))
+fallecidos_covid <- na.omit(fallecidos_covid)
+sapply(positivos_covid, function(x) sum(is.na(x)))
+fallecidos_covid <- na.omit(positivos_covid)
+
+summary(fallecidos_covid$EDAD_DECLARADA)
+assert_all_are_in_closed_range(fallecidos_covid$EDAD_DECLARADA, lower = 0, upper = 107)
+summary(positivos_covid$EDAD)
+assert_all_are_in_closed_range(positivos_covid$EDAD, lower = 0, upper = 120)
+
+assert_all_are_in_past(fallecidos_covid$FECHA_FALLECIMIENTO)
+assert_all_are_in_past(fallecidos_covid$FECHA_NAC)
+assert_all_are_in_past(positivos_covid$FECHA_RESULTADO)
+fallecidos_covid %>% filter(FECHA_NAC>today())
+
+sum(duplicated(fallecidos_covid))
+sum(duplicated(positivos_covid))
+
+unique(fallecidos_covid$DEPARTAMENTO)
+
+fallecidos_covid %>% group_by(SEXO, EDAD_DECLARADA) %>% count()  #no funciona mi dplyr?
+
+#------------------------------------------------------------------
+
+View(fallecidos_covid)
+sapply(fallecidos_covid,class)
+table(fallecidos_covid$SEXO)
+table(fallecidos_covid$DEPARTAMENTO)
+sapply(fallecidos_covid,class)
 
 ## Promedio de edades y numero de muertos por departamento
 
-fallecidos %>% 
+fallecidos_covid %>% 
   group_by(`DEPARTAMENTO`,`SEXO`) %>%
   summarise(promedio_edad=mean(`EDAD_DECLARADA`),numero=n()) %>%
   View()
 
 ## Analizamos mas a profundidad los distritos de nuestro departamento ?
 
-fallecidos %>%
+fallecidos_covid %>%
   filter(`DEPARTAMENTO`=="LIMA")%>%
   group_by(`DISTRITO`)%>%
   summarise(promedio_edad=mean(`EDAD_DECLARADA`),numero=n()) %>%
   View()
 
-fallecidos_depar<-fallecidos %>%
+fallecidos_depar<-fallecidos_covid %>%
   select(FECHA_FALLECIMIENTO,DEPARTAMENTO,EDAD_DECLARADA) %>%
   filter(`DEPARTAMENTO`=="LIMA")
 View(fallecidos_depar)
@@ -54,7 +110,7 @@ grafico<-ggplot(fallecidos_depar,aes(x=FECHA_FALLECIMIENTO,y=EDAD_DECLARADA))+
   
 ggplotly(grafico) %>% layout(legend = list(orientation = "h", x = 0.4, y = -0.2))
 
-#REYNALDO
+#-----------------REYNALDO
 library(tidyverse)
 library(tint)
 library(ggthemes)
